e58735900bb39959e39c95f48b8454d130e65824
Andree Wijaya <andreewijaya09@gmail.com>
2016-08-09 09:44:32 +0700

    adding photo album controller

---
+++ virtualTourist/PhotoAlbumViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoAlbumViewController.swift
+//  virtualTourist
+//
+//  Created by Andree Wijaya on 8/9/16.
+//  Copyright © 2016 Andree Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class PhotoAlbumViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
